39fb174daf85b75b27ffe59cb548f71a3284337f
Felix Lucas <felixlucas38@gmail.com>
2018-12-11 00:21:33 -0300

    Add serve static files in prod and cleaning

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const config = require('config');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -55,6 +56,14 @@ Fawn.init(mongoose);
 
 // --- Routes
 app.use(require('./routes'));
+
+// --- Serve static content
+if (isProduction) {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // --- Error Handlers
 if (!isProduction) {
